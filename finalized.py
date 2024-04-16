e626bad7103df09a56a861b0df33514e73f07b66
Guanyi Cao <93228366+gca0@users.noreply.github.com>
2024-04-16 15:19:21 +0800

    Update finalized.py

--- finalized.py
+++ finalized.py
@@ -753,7 +753,6 @@ def main():
         <!DOCTYPE html>
         <html>
         <head>
-            # <meta name="color-scheme" content="light dark">
             <!-- CSS -->
             <style>
             {css2}
